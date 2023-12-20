e85eadcf069eacd65141066cce29bf4b8bfb2194
Oliver Gomez <kayserenrique@gmail.com>
2023-12-20 19:37:33 -0400

    editar jugador

--- resources/views/livewire/gremiolinhir.blade.php
+++ resources/views/livewire/gremiolinhir.blade.php
@@ -256,15 +256,17 @@
                                                     </svg>
                                                 </a>
                                             </div>
-                                            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110"
-                                                wire:click="">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                    stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                        d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                                </svg>
-                                            </div>
-                                            @if (@Auth::user()->hasPermissionTo('eliminar_jugador'))
+                                            @if (@Auth::user()->hasPermissionTo('editar_jugador'))
+                                                <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110"
+                                                    wire:click="">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                        stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                                    </svg>
+                                                </div>
+                                            @endif
+                                            @if (@Auth::user()->hasPermissionTo('eliminar_jugador'))                                            
                                                 <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110"
                                                     wire:click="consultaborrarintegrante({{$miembro->id}})">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
