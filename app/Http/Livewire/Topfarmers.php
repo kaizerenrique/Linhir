493874b6d132b4dbf77da33e7b748683529eb3ab
Oliver Gomez <kayserenrique@gmail.com>
2024-01-11 21:00:05 -0400

    top farmers

--- app/Http/Livewire/Topfarmers.php
+++ app/Http/Livewire/Topfarmers.php
@@ -3,11 +3,89 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use \App\Traits\AlbionOnline\Gremio;
 
 class Topfarmers extends Component
 {
+    use Gremio;
+
     public function render()
     {
-        return view('livewire.topfarmers');
+        $tops = $this->topfarmers();
+
+        //dd($tops);
+
+        
+
+        return view('livewire.topfarmers',[
+            'tops' => $tops,
+        ]);
+    }
+
+    public function topfarmers()
+    {
+        $linhir_id = config('app.linhir_gremio_id');
+        $integrantes = $this->integrantesdelgremio($linhir_id);
+
+        
+
+        foreach ($integrantes as $integrante) {
+           $cosechadorer[] = [
+            'Cosechador' => $integrante->LifetimeStatistics->Gathering->Fiber->Total,
+            'Name' => $integrante->Name,            
+           ];
+
+           $peleteros[] = [
+            'Peletero' => $integrante->LifetimeStatistics->Gathering->Hide->Total,
+            'Name' => $integrante->Name,            
+           ];
+
+           $prospectors[] = [
+            'Prospector' => $integrante->LifetimeStatistics->Gathering->Ore->Total,
+            'Name' => $integrante->Name, 
+           ];
+
+           $canteros[] = [
+            'Cantero' => $integrante->LifetimeStatistics->Gathering->Rock->Total,
+            'Name' => $integrante->Name, 
+           ];
+
+           $leñadores[] = [
+            'Leñador' => $integrante->LifetimeStatistics->Gathering->Wood->Total,
+            'Name' => $integrante->Name, 
+           ];
+
+           $pescadores[] = [
+            'Pescador' => $integrante->LifetimeStatistics->FishingFame,
+            'Name' => $integrante->Name, 
+           ];
+
+           $agricultores[] = [
+            'Agricultor' => $integrante->LifetimeStatistics->FarmingFame,
+            'Name' => $integrante->Name, 
+           ];
+
+           $crafters[] = [
+            'Crafter' => $integrante->LifetimeStatistics->Crafting->Total,
+            'Name' => $integrante->Name, 
+           ];
+        }
+
+        //$cocechador_l = max($cosechadorer);
+
+        $top_gremio = [
+            'Cosechador' => max($cosechadorer),
+            'Peletero' => max($peleteros),
+            'Prospector' => max($prospectors),
+            'Cantero' => max($canteros),
+            'Leñador' => max($leñadores),
+            'Pescador' => max($pescadores),
+            'Agricultor' => max($agricultores),
+            'Crafter' => max($crafters),
+        ];
+
+        //$collection = collect($top_gremio);
+        
+        return $top_gremio;         
     }
 }
--- resources/views/livewire/topfarmers.blade.php
+++ resources/views/livewire/topfarmers.blade.php
@@ -1,63 +1,72 @@
 <section class="bg-white dark:bg-gray-900">
     <div class="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
-        <h2 class="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl">Top del Gremio </h2>
+        <h2 class="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl">Top Farmers de Linhir </h2>
         <div class="grid grid-cols-2 gap-8 text-gray-500 sm:gap-12 md:grid-cols-3 lg:grid-cols-4 dark:text-gray-400">
 
             <div class="flex items-start p-4 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="ml-4">
-                  <h2 class="font-semibold text-gray-800 dark:text-gray-200 leading-tight">Cosechador</h2>
-                  <p class="mt-2 text-sm dark:text-gray-200"> P </p>
-                </div>
-            </div>
-
-            <div class="flex items-start p-4 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="ml-4">
-                  <h2 class="font-semibold text-gray-800 dark:text-gray-200 leading-tight">Peletero</h2>
-                  <p class="mt-2 text-sm dark:text-gray-200"> P </p>
-                </div>
-            </div>
-            
-            <div class="flex items-start p-4 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="ml-4">
-                  <h2 class="font-semibold text-gray-800 dark:text-gray-200 leading-tight">Prospector</h2>
-                  <p class="mt-2 text-sm dark:text-gray-200"> P </p>
+                  <h2 class="font-semibold text-gray-800 dark:text-gray-200 leading-tight">Agricultor</h2>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{$tops["Agricultor"]["Name"]}} </p>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{number_format($tops["Agricultor"]["Agricultor"])}} </p>
                 </div>
             </div>
 
             <div class="flex items-start p-4 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="ml-4">
                   <h2 class="font-semibold text-gray-800 dark:text-gray-200 leading-tight">Cantero</h2>
-                  <p class="mt-2 text-sm dark:text-gray-200"> P </p>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{$tops["Cantero"]["Name"]}} </p>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{number_format($tops["Cantero"]["Cantero"])}} </p>
                 </div>
             </div>
 
             <div class="flex items-start p-4 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="ml-4">
+                  <h2 class="font-semibold text-gray-800 dark:text-gray-200 leading-tight">Cosechador</h2>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{$tops["Cosechador"]["Name"]}} </p>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{number_format($tops["Cosechador"]["Cosechador"])}} </p>
+                </div>
+            </div>
+
+            <div class="flex items-start p-4 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="ml-4">
+                  <h2 class="font-semibold text-gray-800 dark:text-gray-200 leading-tight">Crafter</h2>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{$tops["Crafter"]["Name"]}} </p>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{number_format($tops["Crafter"]["Crafter"])}} </p>
+                </div>
+            </div>
+
+            <div class="flex items-start p-4 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="ml-4">
+                  <h2 class="font-semibold text-gray-800 dark:text-gray-200 leading-tight">Peletero</h2>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{$tops["Peletero"]["Name"]}} </p>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{number_format($tops["Peletero"]["Peletero"])}} </p>
+                </div>
+            </div>
+            
+            <div class="flex items-start p-4 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="ml-4">
+                  <h2 class="font-semibold text-gray-800 dark:text-gray-200 leading-tight">Prospector</h2>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{$tops["Prospector"]["Name"]}} </p>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{number_format($tops["Prospector"]["Prospector"])}} </p>
+                </div>
+            </div>            
+
+            <div class="flex items-start p-4 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="ml-4">
                   <h2 class="font-semibold text-gray-800 dark:text-gray-200 leading-tight">Leñador</h2>
-                  <p class="mt-2 text-sm dark:text-gray-200"> P </p>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{$tops["Leñador"]["Name"]}} </p>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{number_format($tops["Leñador"]["Leñador"])}} </p>
                 </div>
             </div>
 
             <div class="flex items-start p-4 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="ml-4">
                   <h2 class="font-semibold text-gray-800 dark:text-gray-200 leading-tight">Pescador</h2>
-                  <p class="mt-2 text-sm dark:text-gray-200"> P </p>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{$tops["Pescador"]["Name"]}} </p>
+                  <p class="mt-2 text-sm dark:text-gray-200">{{number_format($tops["Pescador"]["Pescador"])}} </p>
                 </div>
-            </div>
-
-            <div class="flex items-start p-4 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="ml-4">
-                  <h2 class="font-semibold text-gray-800 dark:text-gray-200 leading-tight">Agricultor</h2>
-                  <p class="mt-2 text-sm dark:text-gray-200"> P </p>
-                </div>
-            </div>
-
-            <div class="flex items-start p-4 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="ml-4">
-                  <h2 class="font-semibold text-gray-800 dark:text-gray-200 leading-tight">Elaboración</h2>
-                  <p class="mt-2 text-sm dark:text-gray-200"> P </p>
-                </div>
-            </div>
+            </div>  
+            
         </div>
     </div>
 </section>
