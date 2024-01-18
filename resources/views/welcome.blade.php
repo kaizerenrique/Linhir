42085c63f4361be6aaf1c0b6c75b1e47c32b5180
Oliver Gomez <kayserenrique@gmail.com>
2024-01-18 15:18:17 -0400

    favicon e imagenes de componentes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('/plantilla/favicon.ico') }}">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
