ea5227dcf57a8d554e8367fc03f7dfeaca3a678a
Oliver Gomez <kayserenrique@gmail.com>
2024-02-06 16:22:56 -0400

    prueba

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('consulta:revisareventos')->everyThreeMinutes();
 
         //envia una notificacion diaria con una actividad definida
-        $schedule->command('actividad:actividaldeldia')->dailyAt('20:16');
+        $schedule->command('actividad:actividaldeldia')->dailyAt('20:25');
     }
 
     /**
